5dec426a517d0487276c897408c11758366b9fec
Cheyu0323 <zxzx44585@gmail.com>
2022-12-02 17:21:59 +0800

    fix image path

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+const prod = process.env.NODE_ENV === "production";
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true
-  },
-  basePath: "/seaweed-house",
-  assetPrefix: "/seaweed-house"
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: [prod ? "/seaweed-house" : "/"], 
+      unoptimized: true,
+    },
+    basePath: prod ? "/seaweed-house" : "/",
+    assetPrefix: prod ? "/seaweed-house" : "/",
+    
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
