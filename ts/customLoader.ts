269505c328d66e192849a347de41f9fba396c0d0
Cheyu0323 <zxzx44585@gmail.com>
2022-12-14 14:25:11 +0800

    photo optimization

--- ts/customLoader.ts
+++ ts/customLoader.ts
@@ -1,4 +1,4 @@
 const customLoader = ({ src, width, quality }: { src: string, width: number, quality?: number }) => {
-    return `${src}?w=${width}&q=${quality || 75}`;
+    return `${src}?w=${width}&q=${quality || 75}&?fm=webp`;
 };
 export default customLoader
